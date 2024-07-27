f65554af1f22f01cfc849d51a490ff2c67860ba4
Ueslei Jesus <105524439+eUesleiJesus@users.noreply.github.com>
2024-07-27 12:04:05 -0300

    ativ01

---
+++ Ativ01_JS/conectar.js
@@ -0,0 +1,20 @@
+//document.addEventListener('DOMContentLoaded', function() {
+//    function conectar1() {
+//      console.log("Conexão feita com sucesso!");
+//    }
+  
+//    conectar1();
+//    conectar1();
+//    conectar1();
+// });
+
+// script2.js
+function conectar2() {
+    console.log("Conexão feita com sucesso!");
+  }
+  
+  window.addEventListener('load', function() {
+    conectar2();
+    conectar2();
+    conectar2();
+  });
